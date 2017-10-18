58fbd6093f0a6b847bfd3f4e3630d644e9af08a2
Andrew Hatfield <andrew8412@gmail.com>
2017-10-18 16:08:48 -0400

    finished sum.js

--- sum.js
+++ sum.js
@@ -1,7 +1,27 @@
-if(process.argv[2] === 'sum') {
-    var num1 = parseInt(process.argv[3]);
-    var num2 = parseInt(process.argv[4]);
-    console.log('');
-    console.log('');
-    console.log(num1 + num2);
-}
+// grab input arguments
+var arg = process.argv.splice(2);
+// console.log(arg); // [ '23', '3', '034', 'hello', '93' ]
+
+// A loop that adds up the numbers and skips over not-numbers
+var sum = 0;
+arg.forEach(function(e) {
+    var num = parseInt(e)
+    if(isNaN(num) === true) {
+        return;
+    } else {
+        sum += parseInt(e);
+    }
+});
+
+console.log('\x1b[34m','==========================================================', '\x1b[0m');
+console.log('');
+console.log('\x1b[1m','\x1b[30m',`${arg.join(' + ')}`);
+console.log('\x1b[1m','\x1b[32m',`                      = ${sum}`, '\x1b[0m');
+console.log('');
+console.log('\x1b[34m','==========================================================', '\x1b[0m');
+
+
+
+
+
+
