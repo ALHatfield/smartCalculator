2a8cd06281bea56f69f3821a7a5ad54799e524be
Andrew Hatfield <andrew8412@gmail.com>
2017-10-18 19:46:09 -0400

    added mult.js

---
+++ mult.js
@@ -0,0 +1,10 @@
+function mult() {
+    // Grab the numeral input arguments
+    var arg = process.argv.splice(3);
+    // Initiate product to first argument
+    var product = arg[0];
+    // Loop that subtracts each number and skips over non-numbers
+    for(var i = arg.length -2; i >= 0; i--) {
+        
+    }
+}
