2eec7c56c8e471266355a2c08d1194a22b1dc7e0
Andrew Hatfield <andrew8412@gmail.com>
2017-10-18 14:24:34 -0400

    sum.js changes

--- sum.js
+++ sum.js
@@ -1,4 +1,7 @@
 if(process.argv[2] === 'sum') {
     var num1 = parseInt(process.argv[3]);
     var num2 = parseInt(process.argv[4]);
+    console.log('');
+    console.log('');
+    console.log(num1 + num2);
 }
